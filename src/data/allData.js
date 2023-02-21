a29925b8e6a1f8dc830bab05b8660817f1403bf0
Trung <trungtd@perfin.io>
2023-02-21 14:49:50 +0700

    up

--- src/data/allData.js
+++ src/data/allData.js
@@ -3,15 +3,22 @@ export const data = [
         graftOne: [
             {
                 id: 1,
-                question: "Bản thiết kế sự sống được giấu ở đâu?",
-                options: ["A. Kinh Phật", "B. Kinh Thánh", "C. Kinh Dịch", "D. Tất cả các đáp án trên"],
-                answer: "B. Kinh Thánh"
+                questionText: "Bản thiết kế sự sống được giấu ở đâu?",
+                answers: [
+                    { answerText: "A. Kinh Phật", isCorrect: false },
+                    { answerText: "B. Kinh Thánh", isCorrect: false },
+                    { answerText: "C. Kinh Dịch", isCorrect: true },
+                    { answerText: "D. Tất cả các đáp án trên", isCorrect: false }
+                ],
             },
             {
                 id: 2,
-                question: "Đức Chúa Trời đã khóa sự mầu nhiệm bản thiết kế sự sống bằng gì?",
-                options: ["A. Lời ví dụ", "B. Ổ khóa và chìa khóa", "C. Sách thế gian", "D. Không khóa"],
-                answer: "A. Lời ví dụ"
+                answers: [
+                    { answerText: "A. Kinh Phật", isCorrect: false },
+                    { answerText: "B. Kinh Thánh", isCorrect: false },
+                    { answerText: "C. Kinh Dịch", isCorrect: true },
+                    { answerText: "D. Tất cả các đáp án trên", isCorrect: false }
+                ],
             }
         ]
     }, {
@@ -35,8 +42,5 @@ export const data = [
     },
     {
         graftSeven: []
-    },
-    {
-        graftEight: []
     }
 ]
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,9 +1,37 @@
 import { data } from '../../data/allData'
+import { useContext } from 'react'
+import { UserContext } from '../../context/context'
+import './HomePage.scss'
 
 const HomePage = () => {
-    console.log("data", data)
+    const { logout } = useContext(UserContext)
+
+    // Handlers
+    const handleLoggout = async (e) => {
+        e.preventDefault()
+        try {
+            await logout()
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
     return (
-        <div></div>
+        <div className="homePage">
+            <header>
+                <div className="mt-4 d-flex gap-2">
+                    <button className="btn-home bg-success">Trang chủ</button>
+                    <button className="btn-home bg-warning" onClick={handleLoggout}>Đăng xuất</button>
+                </div>
+            </header>
+            <h3 className="heading-topic mt-5">Danh sách chủ đề</h3>
+            <div className="list">
+                {data.map((item, index) => (
+                    <div key={index} >{index + 1}</div>
+                ))}
+            </div>
+        </div>
     )
 }
 
